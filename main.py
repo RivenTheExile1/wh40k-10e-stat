cfe2522c64ff690172737e0cc4edda5bb7ef6772
Connor Ferwerda <59212072+RivenTheExile1@users.noreply.github.com>
2023-10-24 10:31:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,10 @@
 #TODO: work base stats in
 
 #w
-def main():
+def stat_check(attacker, defender):
+    print(attacker)
+    print(defender)
 
-
-
-
-marine = {
-    "Pts" : 20,
-    "T" : 4,
-    "AS" : 3,
-    "W" : 2,
-    "Weapons" : ["Bolt_Rifle", Close_Combat_Weapon"] 
-}
 
 Bolt_Rifle = {
     "BS" : 3,
@@ -29,19 +21,20 @@ Close_Combat_Weapon = {
     "AP" : 0,
     "D" : 1
 }
-Ork_Nob = {
-  "Pts : 14,
-  "T" : 5,
-  "AS" : 5,
-  "W" : 2,
-  "Weapons" : ["Choppa", "Slugga"]
+marine = {
+    "Pts" : 20,
+    "T" : 4,
+    "AS" : 3,
+    "W" : 2,
+    "Weapons" : [Bolt_Rifle, Close_Combat_Weapon] 
 }
+
 Choppa = {
   "WS":3,
   "A":3,
   "S":5,
   "AP":-1,
-  "D":3
+  "D":2
 }
 Slugga = {
   "BS":5,
@@ -50,3 +43,13 @@ Slugga = {
   "AP":0,
   "D":1
 }  
+Ork_Nob = {
+  "Pts : 14,
+  "T" : 5,
+  "AS" : 5,
+  "W" : 2,
+  "Weapons" : [Choppa, Slugga]
+}
+
+
+stat_check(marine, Ork_Nob)
