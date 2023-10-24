f616c6218ce87feeadf5b4e1b6dadea0b7c875f0
Connor Ferwerda <59212072+RivenTheExile1@users.noreply.github.com>
2023-10-24 09:21:48 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+#main file
+#TODO: work base stats in
+
+#w
+def main():
+
+
+
+
+marine = {
+    "Pts" : 20,
+    "T" : 4,
+    "AS" : 3,
+    "W" : 2,
+    "Weapons" : ["Bolt_Rifle", Close_Combat_Weapon"] 
+}
+
+Bolt_Rifle = {
+    "BS" : 3,
+    "A" : 2,
+    "S" : 4,
+    "AP" : -1,
+    "D" : 1
+}
+Close_Combat_Weapon = {
+    "WS" : 3,
+    "A" : 1,
+    "S" : 4,
+    "AP" : 0,
+    "D" : 1
+}
+Ork_Nob = {
+  "Pts : 14,
+  "T" : 5,
+  "AS" : 5,
+  "W" : 2,
+  "Weapons" : ["Choppa", "Slugga"]
+}
+Choppa = {
+  "WS":3,
+  "A":3,
+  "S":5,
+  "AP":-1,
+  "D":3
+}
+Slugga = {
+  "BS":5,
+  "A":1,
+  "S":4,
+  "AP":0,
+  "D":1
+}  
